6efaccd002b25c9b85808868e9cb214095c497de
Lxr3Nz000 <154972993+Lxr3Nz000@users.noreply.github.com>
2025-05-12 16:35:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const portfolioItems = document.querySelectorAll('.portfolio-item');
+document.addEventListener('DOMContentLoaded', function () {
+  const portfolioItems = document.querySelectorAll('.portfolio-item');
 
-    portfolioItems.forEach(item => {
-        item.addEventListener('click', function() {
-            const pdfUrl = this.getAttribute('data-pdf-url');
-            const websiteUrl = this.getAttribute('data-url');
-            const dataType = this.getAttribute('data-type');
+  portfolioItems.forEach(item => {
+    item.addEventListener('click', function () {
+      const pdfUrl = this.getAttribute('data-pdf-url');
+      const websiteUrl = this.getAttribute('data-url');
+      const dataType = this.getAttribute('data-type');
 
-            if (pdfUrl) {
-                window.open(pdfUrl, '_blank');
-            } else if (websiteUrl) {
-                window.open(websiteUrl, '_blank');
-            } else {
-                console.error('Nessun URL (PDF o sito web) trovato per questo lavoro.');
-            }
-        });
+      if (pdfUrl) {
+        window.open(pdfUrl, '_blank');
+      } else if (websiteUrl) {
+        window.open(websiteUrl, '_blank');
+      } else {
+        console.error('Nessun URL (PDF o sito web) trovato per questo lavoro.');
+      }
     });
+  });
 });
 
 const modal = document.getElementById("imageModal");
@@ -47,5 +47,36 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     modal.style.display = "none";
   }
+});
+document.getElementById('alcoholForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const sex = document.getElementById('sex').value;
+  const weight = parseFloat(document.getElementById('weight').value);
+  const alcohol = parseFloat(document.getElementById('alcohol').value);
+  const hours = parseFloat(document.getElementById('hours').value);
+
+  const r = sex === 'male' ? 0.55 : 0.68;
+  const beta = 0.017;
+
+  const BAC = (alcohol / (r * weight)) - (beta * hours);
+
+  let resultMessage = `
+            <table>
+                <tr>
+                    <th>Parametro</th>
+                    <th>Valore</th>
+                </tr>
+                <tr>
+                    <td>Tasso Alcolemico (BAC)</td>
+                    <td>${BAC.toFixed(4)}</td>
+                </tr>
+            </table>`;
+
+  if (BAC >= 0.08) {
+    resultMessage += "<p><strong>Attenzione!</strong> Se guidi, sei oltre il limite legale.</p>";
+  }
+
+  document.getElementById('result').innerHTML = resultMessage;
 });
 
